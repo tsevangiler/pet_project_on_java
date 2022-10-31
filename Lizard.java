0c78130f8da832e4debb7418ce85823f24e9568b
tsevangiler <110197570+tsevangiler@users.noreply.github.com>
2022-11-01 00:00:20 +0300

    Add files via upload

---
+++ Lizard.java
@@ -0,0 +1,6 @@
+public class Lizard extends Animal {
+    public void height(String s, int n) {
+        System.out.println("У меня есть хвост");
+        System.out.println(s + n + " см");
+    }
+}
