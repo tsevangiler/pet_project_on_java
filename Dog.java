1a3c830c4df14be0fb92f412cc4c07b9f66ed393
tsevangiler <110197570+tsevangiler@users.noreply.github.com>
2022-10-31 23:59:10 +0300

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+    @Override
+    public void voice() {
+        super.voice();
+    }
+
+    @Override
+    public void voice(String s) {
+        super.voice(s);
+    }
+}
