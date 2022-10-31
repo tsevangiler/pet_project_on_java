1a0101a74f5a438561fcc5731b7d79477b6dd180
tsevangiler <110197570+tsevangiler@users.noreply.github.com>
2022-10-31 23:54:31 +0300

    методы класса Animal

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    public void voice() {
+        System.out.println("мур");
+    }
+    public void voice(String s) {
+        System.out.println(s);
+    }
+    public void height(String s, int n) {
+        System.out.println(s + n);
+    }
+
+}
